a1bc1ce326bd1c3dae9982c28cf31d9a73315b28
maemoun <maeskhai@maeskhai.>
2025-08-24 20:20:40 +0100

    shell

---
+++ execution/ft_heredoc.c
@@ -0,0 +1,128 @@
+#include ".././parsing/main.h"
+
+void	init_herdoc_fds(t_command *cmd)
+{
+	t_command *cur;
+
+	cur = cmd;
+	while (cur)
+	{
+		cur->fd_herdoc[0] = -1;
+		cur->fd_herdoc[1] = -1;
+		cur = cur->next_command;
+	}
+}
+
+bool	ft_create_herdoc(t_command *cmd, char *name)
+{
+	char *line;
+
+	if (pipe(cmd->fd_herdoc) == -1)
+	{
+		perror("pipe");
+		return (false);
+	}
+	while (1)
+	{
+		line = readline("> ");
+		if (!line)
+		{
+			ft_putstr_fd("warning: heredoc delimited by EOF\n", 2);
+			break;
+		}
+		if (ft_strcmpp(line, name) == 0)
+		{
+			free(line);
+			break;
+		}
+		ft_putendl_fd(line, cmd->fd_herdoc[1]);
+		free(line);
+	}
+	close_fd(&cmd->fd_herdoc[1]);
+	cmd->fd_herdoc[1] = -1;
+	return (true);
+}
+
+// void	herdoc_read(t_command *cmd, char *name, t_env *env_list, t_red_type type)
+// {
+// 	char	*line;
+
+// 	name = ft_strjoin_2(name, "\n");
+// 	if (!name)
+// 		return ;
+// 	close(cmd->fd_herdoc[0]);
+// 	while (1)
+// 	{
+// 		ft_putstr_fd2("> ", 1);
+// 		line = get_next_line(STDIN_FILENO);
+// 		if (!line)
+// 			break ;
+// 		if (!ft_check_strcmp(line, name))
+// 			break ;
+// 		if (expand == D_HERDOC_Q)
+// 			ft_putstr_fd2(line, cmd->fd_herdoc[1]);
+// 		else
+// 			expand_cmd_heredoc(&line, cmd, cmd->fd_herdoc[1]);
+// 		free(line);
+// 	}
+// 	if (line)
+// 		free(line);
+// 	free(name);
+// 	close(tmp->fd_herdoc[1]);
+// }
+
+// bool	ft_create_herdoc(t_command *cmd, char *name, t_env *env_list, t_red_type type)
+// {
+// 	int	fork_pid;
+
+// 	fork_pid = fork();
+// 	if (fork_pid < 0)
+// 	{
+// 		close_fd(&cmd->fd_herdoc[0]);
+// 		close_fd(&cmd->fd_herdoc[1]);
+// 		return (print_error(errno, NULL, NULL), false);
+// 	}
+// 	if (fork_pid == 0)
+// 	{
+// 		signal(SIGINT, ctlc_handler);
+// 		herdoc_read(cmd, name, env_list, type);
+// 		free_cmd_list(cmd);
+// 		free_list(&env_list);
+// 		exit(0);
+// 	}
+// 	if (fork_pid > 0)
+// 	{
+// 		close_fd(&cmd->fd_herdoc[1]);
+// 		if (wait_and_exit(fork_pid))
+// 			return (false);
+// 	}
+// 	return (true);
+// }
+
+bool	ft_process_heredocs(t_command *cmd)
+{
+	t_command		*tmp;
+	t_redirection	*redi;
+
+	tmp = cmd;
+	init_herdoc_fds(cmd);
+	while (tmp)
+	{
+		redi = tmp->red;
+		while (redi)
+		{
+			if (redi->type == RED_HEREDOC)
+			{
+				if (tmp->fd_herdoc[0] > 0)
+					close_fd(&tmp->fd_herdoc[0]);
+				if (tmp->fd_herdoc[1] > 0)
+					close_fd(&tmp->fd_herdoc[1]);
+				if (!ft_create_herdoc(tmp, redi->name))
+					return (close_fd(&tmp->fd_herdoc[0]), close_fd(&tmp->fd_herdoc[1]), false);
+			}
+			redi = redi->next_red;
+		}
+		tmp = tmp->next_command;
+	}
+	return (true);
+}
---
+++ parsing/command_core/extract_all_commands.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   extract_all_commands.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdo <abdo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/16 17:13:58 by abdo              #+#    #+#             */
+/*   Updated: 2025/08/21 15:09:14 by abdo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../main.h"
+
+static t_command	*init_command(void)
+{
+	t_command	*cmd;
+
+	cmd = malloc(sizeof(t_command));
+	if (!cmd)
+		return (NULL);
+	cmd->command = NULL;
+	cmd->red = NULL;
+	cmd->next_command = NULL;
+	return (cmd);
+}
+
+static void	append_command(t_command **head, t_command **last,
+								t_command *new_cmd)
+{
+	if (!*head)
+		*head = new_cmd;
+	else
+		(*last)->next_command = new_cmd;
+	*last = new_cmd;
+}
+
+static void	skip_to_next_pipe(t_token **tokens)
+{
+	while (*tokens && (*tokens)->type != PIPE)
+		*tokens = (*tokens)->next;
+	if (*tokens && (*tokens)->type == PIPE)
+		*tokens = (*tokens)->next;
+}
+
+t_command	*extract_all_commands(t_token *tokens, t_env *env, t_data *data)
+{
+	t_command	*head;
+	t_command	*last;
+	t_command	*new_cmd;
+	t_token		*scan;
+
+	head = NULL;
+	last = NULL;
+	while (tokens)
+	{
+		new_cmd = init_command();
+		if (!new_cmd)
+			return (NULL);
+		scan = tokens;
+		new_cmd->command = get_command_args(&tokens);
+		new_cmd->red = get_redirections(&scan, env, data);
+		append_command(&head, &last, new_cmd);
+		skip_to_next_pipe(&tokens);
+	}
+	return (head);
+}
